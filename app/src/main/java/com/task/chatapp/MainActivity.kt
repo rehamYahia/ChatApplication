ae43ef90353659df58726f61084b8f8d9eac4777
AbdallahAlsayed744 <abdallah@email.com>
2024-06-08 20:41:44 +0300

    Add splash screen for devices above version 12

--- app/src/main/java/com/task/chatapp/MainActivity.kt
+++ app/src/main/java/com/task/chatapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.task.chatapp
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.FirebaseApp
 import com.task.chatapp.ui.theme.ChatAppTheme
@@ -17,7 +21,7 @@ import com.task.chatapp.ui.theme.ChatAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        insialsplashscreen()
         setContent {
             ChatAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -28,6 +32,29 @@ class MainActivity : ComponentActivity() {
 
                 }
             }
+        }
+    }
+
+    private fun insialsplashscreen() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            // For Android 12 and higher, use the SplashScreen API
+            installSplashScreen()
+            splashScreen.setOnExitAnimationListener { splashscreenview ->
+                val slideup = ObjectAnimator.ofFloat(
+                    splashscreenview,
+                    View.TRANSLATION_Y,
+                    -splashscreenview.height.toFloat()
+                )
+                slideup.interpolator = AnticipateInterpolator()
+                slideup.duration = 1000L
+                slideup.doOnEnd {
+                    splashscreenview.remove()
+                }
+                slideup.start()
+            }
+
+        } else {
+            setTheme(R.style.Theme_ChatApp)
         }
     }
 }
